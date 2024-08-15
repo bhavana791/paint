7580e8dbcb652b44536c254cfba6e526eebfedaf
bhavana madas <80811054+bhavana791@users.noreply.github.com>
2024-08-15 11:51:24 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// overlay menu
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+}
+
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+// lightbox gallery
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+// <!-- Counter Script -->
+$(document).ready(function(){
+    $('.counter').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).data('count')
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
